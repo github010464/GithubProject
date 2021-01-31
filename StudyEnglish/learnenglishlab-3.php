55fd436c4bda4c316147223de7a375d770f4b093
github010464 <73011309+github010464@users.noreply.github.com>
2021-01-31 11:55:00 +0800

    Add files via upload

---
+++ StudyEnglish/learnenglishlab-3.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StudyEnglish.Com</title>
+    <link rel="shortcut icon" type="image/jpg" href="asset/edit--v2.gif"/>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="asset/styles.css">
+</head>
+<body>
+
+<?php
+    include_once 'navbar.php';
+?>
+
+    <div class="container-fluid bg-dark">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-12">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe width="853" height="480" src="https://www.youtube.com/embed/-F8b5t-91rc?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+              <div class="embed-responsive embed-responsive-16by9">           
+                <iframe width="853" height="480" src="https://www.youtube.com/embed/pDpMCh5Tav0?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+          </div> 
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe width="853" height="480" src="https://www.youtube.com/embed/6STn5BwVGKE?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/cJkNIU_dVaA?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div> 
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/z45UdL0WTro?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/8KU2eobDMqs?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/74IjWKpzw94?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/kP_VkuB08qY?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/bY5ChVDRLus?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/LciKb0uuFEc?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/kVZwON5jbRo?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/2mponXmHkUY?list=RDCMUCImiNBzWUNuHBm95F8SyMYA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>        
+    </div>
+    
+<div class="footer">
+  <h4>follow and like us</h4>
+  <img src="asset/instagram-3-xl.png" alt="" style='margin-right:10px;'>
+  <img src="asset/twitter-xl.png" alt="">
+  <img src="asset/facebook-xl.png" alt="">
+  <img src="asset/linkedin-6-xl.png" alt="">	
+  <p>Designed by Ronzkie. Copyright &copy; 2020. All rights reserved.</p>
+</div>
+
+<script>
+    
+    $(document).ready(function () {
+        $('.navbar-light .dmenu').hover(function () {
+                $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+            }, function () {
+                $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+            });
+        }); 
+         
+            $(document).ready(function() {
+            $(".megamenu").on("click", function(e) {
+                e.stopPropagation();
+            });
+        });
+        
+</script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ StudyEnglish/oxfordenglish-3.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StudyEnglish.Com</title>
+    <link rel="shortcut icon" type="image/jpg" href="asset/edit--v2.gif"/>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="asset/styles.css">
+</head>
+<body>
+
+<?php
+    include_once 'navbar.php';
+?>
+    
+    <div class="container-fluid bg-dark">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-12">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe width="853" height="480" src="https://www.youtube.com/embed/iKi4Jy6r-0s?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+              <div class="embed-responsive embed-responsive-16by9">           
+                <iframe width="853" height="480" src="https://www.youtube.com/embed/By94mMolWUI?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+          </div> 
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe width="853" height="480" src="https://www.youtube.com/embed/W-uPiTB877c?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/JWUtR8d6wwA?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div> 
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/CmqOXaSUpFo?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/SDO8mP45hr8?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/CSG4pNY7rT8?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">                    
+                     <iframe width="853" height="480" src="https://www.youtube.com/embed/wC5GPzMb9BE?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">                   
+                     <iframe width="853" height="480" src="https://www.youtube.com/embed/rrSl1bQcnoI?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">                    
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/Ink-Mp4euI8?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/PLC-_TXsrXA?list=PLD6t6ckHsruY_i7_rZhKcRBmXDdawiqUM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="853" height="480" src="https://www.youtube.com/embed/uQIuZ6YJ760" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                </div>
+            </div>
+        </div>        
+    </div>
+    
+<div class="footer">
+  <h4>follow and like us</h4>
+  <img src="asset/instagram-3-xl.png" alt="" style='margin-right:10px;'>
+  <img src="asset/twitter-xl.png" alt="">
+  <img src="asset/facebook-xl.png" alt="">
+  <img src="asset/linkedin-6-xl.png" alt="">	
+  <p>Designed by Ronzkie. Copyright &copy; 2020. All rights reserved.</p>
+</div>
+
+<script>
+    
+    $(document).ready(function () {
+        $('.navbar-light .dmenu').hover(function () {
+                $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+            }, function () {
+                $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+            });
+        }); 
+         
+            $(document).ready(function() {
+            $(".megamenu").on("click", function(e) {
+                e.stopPropagation();
+            });
+        });
+        
+</script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
